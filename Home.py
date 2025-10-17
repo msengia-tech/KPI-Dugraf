5ed4e92f6474320f706bb4a412643df9df3f9754
msengia-tech <martinho@dugraf.com.br>
2025-10-17 09:04:58 -0300

    Commit inicial - Estrutura do KPI-Dugraf

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+from header import show_header
+
+show_header()  # Exibe o cabeçalho padrão
+
+
+# Logo no canto superior esquerdo (na sidebar)
+st.logo("assets/logo_dugraf_branco.png", size="large")
